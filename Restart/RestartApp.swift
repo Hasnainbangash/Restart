9c7d2ce0ef1c17becee5b938601321b7e426c9e3
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-13 15:33:46 +0500

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Elexoft on 13/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
