c247e7f932260a3ab82b15f7e612e8f634df362a
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-13 16:04:35 +0500

    A new property onboarding with the app storage binding is get generated

--- Restart/Screens/ContentView.swift
+++ Restart/Screens/ContentView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    // This property will set up a new onboarding key in app storage. This ap storage is a special SwiftUI property wrapper that will use the user's defaults. under the hood. It's purpose is to store some value on the device's permanent storage by utilizing a get and set method. This is a unique key identifier that we can refer to later on by using this key. We can edit or recall its saved value on the device storage.
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
