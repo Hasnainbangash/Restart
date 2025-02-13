21968b1abd914356b489fbf5d35d61920e796225
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-13 17:18:12 +0500

    Center work little bit done along witht he circle on another circle

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -44,6 +44,19 @@ struct OnboardingView: View {
                 
                 // MARK: - CENTER
                 
+                ZStack {
+                    ZStack {
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 40)
+                            .frame(width: 260, height: 260, alignment: .center)
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 80)
+                            .frame(width: 260, height: 260, alignment: .center)
+                    } //: ZSTACK
+                } //: CENTER
+                
+                Spacer()
+                
                 // MARK: - FOOTER
                 
             } //: VSTACK
