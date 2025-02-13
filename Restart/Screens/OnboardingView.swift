5eab49df13101bca2b210ca8f138648b8ba91558
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-13 16:36:53 +0500

    Button action code is done

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct OnboardingView: View {
     // MARK: - PROPERTY
     
+    // This code is identical to previous one, it seems that we are setting a new value for this property again, but in reality this special app storage property wrapper works differently. This true value will only be added to this property when the ptogram doesnot find the onboarding key in the user default storage. In summary if our program finds the onboarding key in the user default storage, then it will skip this initialization and since we have initiliazed this onboarding key in the previous file(ContentView) therefore the end part of this code will be ignored. So this is how we access the previously stored value of the onboarding key.
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    
     // MARK: - BODY
     
     var body: some View {
@@ -18,7 +21,7 @@ struct OnboardingView: View {
                 .font(.largeTitle)
             
             Button(action: {
-                // Some action
+                isOnboardingViewActive = false
             }) {
                 Text("Start")
             }
